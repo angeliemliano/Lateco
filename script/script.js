5b2ea5dbc0dda33a47c0cfc570b8e70a9f4bbf1f
Emiliano Angeli <angeli.emiliano1@gmail.com>
2022-11-17 19:32:55 -0300

    logos added

--- script/script.js
+++ script/script.js
@@ -12,3 +12,10 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
+var onReCaptcha = function () {
+      if (grecaptcha.getResponse().length !== 0) {
+        var btSubmit = document.getElementById("bt-submit");
+        btSubmit.disabled = false;
+      }
+    };
